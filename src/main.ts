18a0a46d35365cdeef28eedb54f8d99f04536456
artem <artenbert@gmail.com>
2019-06-22 22:33:59 +0300

    add function getAsyncRequest()

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import * as superagent from 'superagent';
 init();
 
 function init(): void {
-  sendAsyncRequest();
+  sendRequest();
+  console.log("get Async Request");
+  getAsyncRequest();
 }
 
-function sendAsyncRequest(): void {
+function sendRequest(): void {
   superagent
     .post(requestOptions.uri)
     .send(requestOptions.body)
@@ -15,9 +17,23 @@ function sendAsyncRequest(): void {
       Object.keys(requestOptions.headers)[0],
       requestOptions.headers['Content-Type'],
     )
-    .end((err: superagent.ResponseError,
-          res: superagent.Response) => {
+    .end((err: superagent.ResponseError, res: superagent.Response) => {
       console.log('err: ', err);
       console.log('res: ', res);
     });
+}
+
+async function getAsyncRequest() {
+  try {
+    const result = await superagent
+      .post(requestOptions.uri)
+      .send(requestOptions.body)
+      .set(
+        Object.keys(requestOptions.headers)[0],
+        requestOptions.headers['Content-Type'],
+      );
+    console.log(result);
+  } catch (err) {
+    console.error('err: ', err);
+  }
 }
