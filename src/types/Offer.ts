36a4eb1611d4ff597307d6d152738af621bb09f7
Artem Beresnev <aberesnev@intermedia.net>
2019-06-28 15:21:24 +0300

    Add type to Offer

---
+++ src/types/Offer.ts
@@ -0,0 +1,469 @@
+export interface Offer {
+  roomType: string;
+  newbuilding: Newbuilding;
+  hasSecurity?: any;
+  drainageType?: any;
+  furniturePresence?: any;
+  drivewayType?: any;
+  hasFurniture: boolean;
+  minPriceTotalRur?: any;
+  pricePerUnitAreaPerYearRurForSearch?: any;
+  archivedDate?: any;
+  layout?: any;
+  publishTerms: PublishTerms;
+  title?: any;
+  hasHeating?: any;
+  workTimeInfo?: any;
+  isRecidivist: boolean;
+  electricityPower?: any;
+  offerType: string;
+  gaLabel: string;
+  leisure?: any;
+  separateWcsCount?: any;
+  propertyType?: any;
+  geo: Geo;
+  chatId?: any;
+  isPaid: boolean;
+  notes: Notes;
+  priceTotalPerMonth?: any;
+  pricePerUnitAreaPerYearRur?: any;
+  demolishedInMoscowProgramm: boolean;
+  roomsArea?: any;
+  vasType?: any;
+  windowsViewType?: any;
+  hasLift?: any;
+  hasEncumbrances?: any;
+  isPro: boolean;
+  pricePerUnitAreaUsd?: any;
+  availableFrom?: any;
+  hasShower?: any;
+  isColorized: boolean;
+  isHiddenByUser: boolean;
+  shareAmount?: any;
+  externalUrl?: any;
+  hasDishwasher?: any;
+  isInHiddenBase: boolean;
+  possibleToChangePermittedUseType?: any;
+  water?: any;
+  petsAllowed: boolean;
+  hasBathhouse?: any;
+  maxArea?: any;
+  isEnabledCallTracking?: any;
+  minPriceTotalRurForSearch?: any;
+  electricity?: any;
+  waterCapacity?: any;
+  hasWater?: any;
+  placementType?: any;
+  exportPdfLink: string;
+  isFairplay: boolean;
+  fullUrl: string;
+  priceTotalPerMonthEur?: any;
+  hasParking?: any;
+  rentByPartsDescription?: any;
+  category: string;
+  hasInternet: boolean;
+  conditionRatingType?: any;
+  categoriesIds: number[];
+  isExcludedFromAction: boolean;
+  videos: any[];
+  kitchenArea: string;
+  cianId: number;
+  minVatPriceTotalPerMonthRur?: any;
+  isRentByParts: boolean;
+  pricePerUnitAreaPerYearUsd?: any;
+  isCustoms?: any;
+  gas?: any;
+  layoutPhoto?: any;
+  isPenthouse?: any;
+  hasEquipment?: any;
+  isAuction: boolean;
+  objectGuid: string;
+  hasLight?: any;
+  repairType: string;
+  hasSecondedDocs?: any;
+  hasDrainage?: any;
+  priceTotalRurForSearch?: any;
+  hasTv: boolean;
+  decoration?: any;
+  hasTeaCoffeeSugar?: any;
+  pricePerUnitAreaEur?: any;
+  inputType?: any;
+  hasBathtub?: any;
+  waterType?: any;
+  hasGarage?: any;
+  canParts?: any;
+  id: number;
+  hasGas?: any;
+  projectDeclarationUrl?: any;
+  isApartments?: any;
+  hasIron?: any;
+  priceTotalUsd?: any;
+  minPriceTotalPerMonthRur?: any;
+  isPremium: boolean;
+  description: string;
+  minArea?: any;
+  monthlyIncome?: any;
+  user: User;
+  publishedUserId: number;
+  bedsCount?: any;
+  wcType?: any;
+  flags: Flags;
+  hasRamp?: any;
+  promoInfo?: any;
+  humanizedTimedelta: string;
+  combinedWcsCount?: any;
+  taxNumber?: any;
+  hasPhone: boolean;
+  hasHairdryer?: any;
+  userId: number;
+  estateType?: any;
+  hasConditioner?: any;
+  electricityType?: any;
+  settlementName?: any;
+  minPriceTotal?: any;
+  allFromOffrep?: any;
+  platform: Platform;
+  businessShoppingCenter?: any;
+  hasPool?: any;
+  isMulti: boolean;
+  accessType?: any;
+  totalArea: string;
+  pricePerUnitArea?: any;
+  drainageCapacity?: any;
+  furniture?: any;
+  allRoomsArea: string;
+  demandJsonQuery?: any;
+  name?: any;
+  childrenAllowed: boolean;
+  hasCheckIn_24?: any;
+  garage?: any;
+  permittedUseType?: any;
+  drainage?: any;
+  minVatPriceTotalRur?: any;
+  status: string;
+  isNew: boolean;
+  floorNumber: number;
+  hasInvestmentProject?: any;
+  hasTransportServices?: any;
+  pricePerUnitAreaRur?: any;
+  floorMaterialType?: any;
+  roomArea?: any;
+  additionalPhoneLinesAllowed?: any;
+  bedroomsCount?: any;
+  externalId?: any;
+  isOccupied?: any;
+  booking?: any;
+  descriptionWordsHighlighted: any[];
+  priceTotalEur?: any;
+  hasWiredInternet?: any;
+  hasElectricity?: any;
+  isTop3: boolean;
+  priceTotalPerMonthUsd?: any;
+  gasPressure?: any;
+  hasFridge: boolean;
+  hasTransfer?: any;
+  district?: any;
+  minPriceTotalPerMonth?: any;
+  gasType?: any;
+  cplModeration?: any;
+  flatType: string;
+  hasJacuzzi?: any;
+  roomsCount: number;
+  pricePerUnitAreaPerMonthRurForSearch?: any;
+  priceTotalRur?: any;
+  addedTimestamp: number;
+  feedboxMultiOfferKey?: any;
+  newobjectPromoLabel?: any;
+  isLegalAddressProvided?: any;
+  isFavorite: boolean;
+  pricePerUnitAreaPerMonth?: any;
+  hasWasher: boolean;
+  isImported: boolean;
+  gasCapacity?: any;
+  withoutClientFee: boolean;
+  phoneLinesCount?: any;
+  phones: Phone[];
+  pricePerUnitAreaPerYearEur?: any;
+  loggiasCount?: any;
+  priceChanges: PriceChange[];
+  specialty: Specialty;
+  pricePerUnitAreaPerYear?: any;
+  hasShopWindows?: any;
+  roomsForSaleCount?: any;
+  vatPriceTotalRur?: any;
+  editDate: Date;
+  wcLocationType?: any;
+  bargainTerms: BargainTerms;
+  dealType: string;
+  similar?: any;
+  priceTotalPerMonthRur?: any;
+  hasKitchenFurniture?: any;
+  isByHomeowner?: any;
+  cianUserId: number;
+  minPriceTotalPerMonthRurForSearch?: any;
+  jkUrl?: any;
+  exportDocLink: string;
+  callTrackingProvider?: any;
+  hasExtinguishingSystem?: any;
+  hasWifi?: any;
+  conditionType?: any;
+  hasExcursions?: any;
+  isDuplicatedDescription: boolean;
+  pricePerUnitAreaPerMonthRur?: any;
+  rosreestrCheck?: any;
+  added: string;
+  priceTotalPerMonthRurForSearch?: any;
+  pricePerUnitAreaRurForSearch?: any;
+  fromDeveloper?: any;
+  hasSafeCustody?: any;
+  balconiesCount?: any;
+  building: Building;
+  land?: any;
+  livingArea: string;
+  photos: Photo[];
+  waterPipesCount?: any;
+  vatPriceTotalPerMonthRur?: any;
+  kp?: any;
+  priceTotal?: any;
+}
+
+interface Newbuilding {
+  showJkReliableFlag?: any;
+  isFromDeveloper?: any;
+  isReliable?: any;
+  name?: any;
+  isBuilderReliableInRegion?: any;
+  isFromBuilder?: any;
+  house?: any;
+  isFromSeller?: any;
+  id?: any;
+}
+
+interface Term {
+  tariffIdentificator?: any;
+  isCompulsory?: any;
+  days: number;
+  services: string[];
+  type: string;
+  dynamicPrice?: any;
+}
+
+interface PublishTerms {
+  autoprolong: boolean;
+  terms: Term[];
+}
+
+interface District {
+  geoType: string;
+  qs: string;
+  locationId: number;
+  name: string;
+  title: string;
+  shortName: string;
+  parentId?: number;
+  type: string;
+  fullName: string;
+  id: number;
+}
+
+interface Underground {
+  geoType: string;
+  qs: string;
+  name: string;
+  title: string;
+  cianId?: any;
+  releaseYear?: any;
+  transportType: string;
+  id: number;
+  underConstruction?: any;
+  lineColor: string;
+  time: number;
+  shortName: string;
+  lineId: number;
+  type: string;
+  fullName: string;
+  isDefault: boolean;
+}
+
+interface Address {
+  geoType: string;
+  qs: string;
+  name: string;
+  title: string;
+  locationTypeId?: number;
+  shortName: string;
+  type: string;
+  fullName: string;
+  isFormingAddress?: boolean;
+  id: number;
+}
+
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+interface Geo {
+  highways: any[];
+  districts: District[];
+  userInput: string;
+  countryId: number;
+  undergrounds: Underground[];
+  jk?: any;
+  address: Address[];
+  buildingAddress?: any;
+  coordinates: Coordinates;
+}
+
+interface Notes {
+  realtor?: any;
+  offer?: any;
+}
+
+interface User {
+  phoneNumbers: any[];
+  userTrustLevel: string;
+  agencyName: string;
+  isBuilder: boolean;
+  agentAvailability?: any;
+  agentAccountType: string;
+  cianUserId: number;
+  isChatsEnabled: boolean;
+  subAgentCompanyName: string;
+  isCallbackUser: boolean;
+  isSubAgent: boolean;
+  agentReviews?: any;
+  isAgent: boolean;
+  profileUri?: any;
+  personalRating?: any;
+  cianProfileStatus: string;
+  isHidden: boolean;
+  agentAvatarUrl?: any;
+}
+
+interface Flags {
+  isArchived: boolean;
+}
+
+interface Platform {
+  version?: any;
+  type: string;
+}
+
+interface Phone {
+  number: string;
+  countryCode: string;
+}
+
+interface PriceData {
+  paymentPeriod: string;
+  currency: string;
+  price: number;
+}
+
+interface PriceChange {
+  changeTime: Date;
+  priceData: PriceData;
+}
+
+interface Specialty {
+  additionalTypes: any[];
+  specialties: any[];
+  types: any[];
+}
+
+interface VatPrices {
+  rur?: any;
+  usd?: any;
+  eur?: any;
+}
+
+interface SecurityDepositPrices {
+  rur?: any;
+  usd?: any;
+  eur?: any;
+}
+
+interface BargainPricePrices {
+  rur?: any;
+  usd?: any;
+  eur?: any;
+}
+
+interface DepositPrices {
+  rur: number;
+  usd: number;
+  eur: number;
+}
+
+interface UtilitiesTermsPrices {
+  rur: number;
+  usd: number;
+  eur: number;
+}
+
+interface UtilitiesTerms {
+  price: number;
+  includedInPrice: boolean;
+}
+
+interface BargainTerms {
+  paymentPeriod: string;
+  mortgageAllowed?: any;
+  vatPrices: VatPrices;
+  agentFee: number;
+  includedOptions: any[];
+  securityDepositPrices: SecurityDepositPrices;
+  price: number;
+  bargainPricePrices: BargainPricePrices;
+  priceRur: number;
+  priceEur: number;
+  currency: string;
+  tenantsType?: any;
+  securityDeposit?: any;
+  depositPrices: DepositPrices;
+  contractType?: any;
+  vatIncluded?: any;
+  utilitiesTermsPrices: UtilitiesTermsPrices;
+  vatType?: any;
+  hasGracePeriod?: any;
+  clientFee: number;
+  vatPrice?: any;
+  saleType?: any;
+  utilitiesTerms: UtilitiesTerms;
+  minLeaseTerm?: any;
+  priceUsd: number;
+  leaseTermType: string;
+  leaseType?: any;
+  deposit: number;
+  bargainPrice?: any;
+  bargainAllowed?: any;
+  bargainConditions?: any;
+  agentBonus?: any;
+  prepayMonths: number;
+  priceType: string;
+}
+
+interface Building {
+  cargoLiftsCount?: any;
+  heatingType?: any;
+  type?: any;
+  deadline?: any;
+  materialType?: any;
+  passengerLiftsCount?: any;
+  floorsCount: number;
+  buildYear?: any;
+  accessType?: any;
+}
+
+interface Photo {
+  source?: any;
+  fullUrl: string;
+  rotateDegree?: any;
+  isDefault: boolean;
+  thumbnailUrl: string;
+  thumbnail2Url: string;
+  id: number;
+  coordinates?: any;
+  miniUrl: string;
+}
