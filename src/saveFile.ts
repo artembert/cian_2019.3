7a76e5fcba554630b03ae66966576865c4bad985
Artem Beresnev <artembert@gmail.com>
2019-11-19 18:58:14 +0300

    fix appendOrSaveFile() fn

--- src/saveFile.ts
+++ src/saveFile.ts
@@ -17,14 +17,12 @@ export async function saveFile(pathToFile: string, data: any): Promise<boolean> 
 export async function appendOrSaveFile(pathToFile: string, data: any): Promise<boolean> {
   console.log('appendToFile()');
   if (fs.existsSync(pathToFile)) {
-    console.log('EXIST');
-    appendFile(pathToFile, JSON.stringify(data), 'utf8');
+    await appendFile(pathToFile, JSON.stringify(data), 'utf8');
     CustomConsole.SYSTEM_INFORMATION(
       `serialized data saved to ${pathToFile}`,
     );
     return true;
   } else {
-    console.log('NEW');
     await writeFile(pathToFile, JSON.stringify(data), 'utf8');
     CustomConsole.SYSTEM_INFORMATION(
       `file ${pathToFile} created`,
