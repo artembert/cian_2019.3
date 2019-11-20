51c027f1e1a9f54e047d96703b85c36042d62b15
Artem Beresnev <artembert@gmail.com>
2019-11-20 12:32:03 +0300

    beautifully JSON

--- src/saveFile.ts
+++ src/saveFile.ts
@@ -7,7 +7,7 @@ const appendFile = util.promisify(fs.appendFile);
 
 export async function saveFile(pathToFile: string, data: any): Promise<boolean> {
   console.log('saveFile()');
-  await writeFile(pathToFile, JSON.stringify(data), 'utf8');
+  await writeFile(pathToFile, JSON.stringify([...data], null, 2), 'utf8');
   CustomConsole.SYSTEM_INFORMATION(
     `file ${pathToFile} created`,
   );
@@ -17,13 +17,13 @@ export async function saveFile(pathToFile: string, data: any): Promise<boolean> 
 export async function appendOrSaveFile(pathToFile: string, data: any): Promise<boolean> {
   console.log('appendToFile()');
   if (fs.existsSync(pathToFile)) {
-    await appendFile(pathToFile, JSON.stringify(data), 'utf8');
+    await appendFile(pathToFile, JSON.stringify([...data], null, 2), 'utf8');
     CustomConsole.SYSTEM_INFORMATION(
       `serialized data saved to ${pathToFile}`,
     );
     return true;
   } else {
-    await writeFile(pathToFile, JSON.stringify(data), 'utf8');
+    await writeFile(pathToFile, JSON.stringify([...data], null, 2), 'utf8');
     CustomConsole.SYSTEM_INFORMATION(
       `file ${pathToFile} created`,
     );
